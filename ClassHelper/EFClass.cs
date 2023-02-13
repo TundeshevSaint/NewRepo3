8f942de00af9c9e7b91884d69ab8e2a538c90897
Имя Фамилия <visualstudio1945@outlook.com>
2023-02-13 13:11:24 +0300

    Добавьте файлы проекта.

---
+++ ClassHelper/EFClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FitnessClubKiselev.DB;
+using FitnessClubKiselev.Windows;
+
+namespace FitnessClubKiselev.ClassHelper
+{
+    public class EFClass
+    {
+        public static Entities context { get; set; } = new Entities();
+    }
+}
+//
